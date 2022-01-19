732687943d2684ceb705a751a8df4d044abc6935
mark <uptree@qq.com>
2022-01-19 11:34:58 +0800

    文件系统函数

--- file/file_test.go
+++ file/file_test.go
@@ -4,51 +4,13 @@ import (
 	"testing"
 )
 
-func TestSelfPath(t *testing.T) {
-	t.Logf("SelfPath: %v", SelfPath())
+func TestWriteBytesToFile(t *testing.T) {
+	path2 := "dir1/dir2/1.txt"
+	t.Logf("WriteBytesToFile: %v", WriteBytesToFile(path2, []byte("hello")))
 }
 
-func TestSelfDir(t *testing.T) {
-	t.Logf("SelfDir: %v", SelfDir())
-}
-
-func TestMkDir(t *testing.T) {
-	path2 := "dir1/dir2/dir3"
-	t.Logf("Mkdir: %v", MkDir(path2))
-}
-
-func TestIsEmpty(t *testing.T) {
-	path2 := "dir1/dir2"
-	t.Logf("IsEmpty: %v", IsEmpty(path2))
-}
-
-func TestIsDir(t *testing.T) {
-	path2 := "dir1/dir2"
-	t.Logf("IsDir: %v", IsDir(path2))
-}
-
-func TestIsFile(t *testing.T) {
-	path2 := "dir1/dir2"
-	t.Logf("IsFile: %v", IsFile(path2))
-}
-
-func TestListIndex(t *testing.T) {
-	path2 := "dir1"
-	files, dirs, err := ListIndex(path2)
-	t.Logf("ListFiles: %v, %v, %v", files, dirs, err)
-}
-
-func TestClearDir(t *testing.T) {
-	path2 := "dir1/dir2/dir3"
-	t.Logf("ClearDir: %v", ClearDir(path2))
-}
-
-func TestClearDirF(t *testing.T) {
-	path2 := "dir1"
-	t.Logf("ClearDirF: %v", ClearDirF(path2))
-}
-
-func TestRemoveDir(t *testing.T) {
-	path2 := "dir1"
-	t.Logf("RemoveDir: %v", RemoveDir(path2))
+func TestReadFileToBytes(t *testing.T) {
+	path2 := "dir1/dir2/1.txt"
+	b, err := ReadFileToBytes(path2)
+	t.Logf("ReadFileToBytes: %v, %v", string(b), err)
 }
