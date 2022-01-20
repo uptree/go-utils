9b05f76bbd18299143356ac0c5520b986e50a8fc
mark <uptree@qq.com>
2022-01-20 16:28:02 +0800

    base64函数

---
+++ encode/base64.go
@@ -0,0 +1,13 @@
+package encode
+
+import "encoding/base64"
+
+// Base64Encode base64 编码
+func Base64Encode(src []byte) string {
+	return base64.StdEncoding.EncodeToString(src)
+}
+
+// Base64Decode base64 解码
+func Base64Decode(src string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(src)
+}
