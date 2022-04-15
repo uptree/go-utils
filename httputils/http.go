8672bd85a24a17042a2db9df4d095f3a5a6fe92d
mark <uptree@qq.com>
2022-04-15 15:31:02 +0800

    http post

---
+++ httputils/http.go
@@ -0,0 +1,26 @@
+package httputils
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func PostWithHeader(url string, msg []byte, headers map[string]string) (string, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(string(msg)))
+	if err != nil {
+		return "", err
+	}
+	for key, header := range headers {
+		req.Header.Set(key, header)
+	}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
