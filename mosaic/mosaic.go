d4b6ae7acf5405efff7dd40058cbd50a8f7bfb48
uptree <uptree@qq.com>
2022-08-20 00:09:27 +0800

    脱敏函数

--- mosaic/mosaic.go
+++ mosaic/mosaic.go
@@ -2,58 +2,59 @@ package mosaic
 
 import "strings"
 
-// SafeIdCard 身份证号脱敏
-func SafeIdCard(idCard string) string {
-	cp := idCard
-	lens := len(cp)
-	return cp[0:4] + " **** **** " + cp[lens-4:]
+// IdCard 身份证号脱敏
+func IdCard(s string) string {
+	return s[0:4] + " **** **** " + s[len(s)-4:]
 }
 
-// SafeMobile 手机号脱敏
-func SafeMobile(mobile string) string {
-	cp := mobile
-	lens := len(cp)
-	return cp[0:3] + "****" + cp[lens-4:]
+// IdCardStrict 严格身份证号脱敏
+func IdCardStrict(s string) string {
+	return s[0:1] + "*** **** **** ***" + s[len(s)-1:]
 }
 
-// Safe 自定义字符脱敏
-func Safe(cp string, front, end int) string {
-	lens := len(cp)
-	return cp[0:front] + strings.Repeat("*", lens-front-end) + cp[lens-end:]
+// Mobile 手机号脱敏
+func Mobile(s string) string {
+	return s[0:3] + "****" + s[len(s)-4:]
 }
 
-// SafeChineseName 中文姓名脱敏
-func SafeChineseName(str string) string {
-	var result string
-	nameRune := []rune(str)
-	lens := len(nameRune)
-	if lens <= 1 {
-		result = "**"
-	} else if lens == 2 {
-		result = string(nameRune[:1]) + "*"
-	} else if lens == 3 {
-		result = string(nameRune[:1]) + "*" + string(nameRune[2:3])
-	} else if lens == 4 {
-		result = string(nameRune[:1]) + "**" + string(nameRune[lens-1:lens])
-	} else if lens > 4 {
-		result = string(nameRune[:1]) + strings.Repeat("*", lens-2) + string(nameRune[lens-1:lens])
+// LastFour 后四位
+func LastFour(s string) string {
+	l := len(s)
+	if l <= 4 {
+		return s
 	}
-	return result
+	return s[l-4:]
 }
 
-// SafeEmail 邮箱脱敏
-func SafeEmail(str string) string {
-	var result string
-	res := strings.Split(str, "@")
-	lens := len(res[0])
-	if lens <= 1 {
-		resString := "*"
-		result = resString + "@" + res[1]
-	} else {
-		rs := []rune(str)
-		res2 := string(rs[0:1])
-		resString := res2 + strings.Repeat("*", lens-1)
-		result = resString + "@" + res[1]
+// Mosaic 自定义字符脱敏
+func Mosaic(cp string, front, end int) string {
+	l := len(cp)
+	return cp[0:front] + strings.Repeat("*", l-front-end) + cp[l-end:]
+}
+
+// ChineseName 中文姓名脱敏
+func ChineseName(s string) string {
+	r := []rune(s)
+	l := len(r)
+	if l == 2 {
+		return "*" + string(r[1:])
+	} else if l == 3 {
+		return "*" + string(r[l-2:])
+	} else if l == 4 {
+		return "**" + string(r[l-2:])
+	} else if l > 4 {
+		return string(r[:1]) + strings.Repeat("*", l-3) + string(r[l-2:])
 	}
-	return result
+	return "**"
+}
+
+// Email 邮箱脱敏
+func Email(s string) string {
+	ss := strings.Split(s, "@")
+	l := len(ss[0])
+	if l <= 1 {
+		return "*@" + ss[1]
+	}
+	r := []rune(s)
+	return string(r[0:1]) + strings.Repeat("*", l-1) + "@" + ss[1]
 }
--- mosaic/mosaic_test.go
+++ mosaic/mosaic_test.go
@@ -5,38 +5,48 @@ import (
 	"testing"
 )
 
-func TestSafe(t *testing.T) {
+func TestMosaic(t *testing.T) {
 	cp := "13844239123"
 	n := len(cp)
-	s := Safe(cp, 1, 2)
+	s := Mosaic(cp, 1, 2)
 	assert.Lenf(t, s, n, "长度:%d", n)
 	assert.Equalf(t, "1********23", s, "failed")
 }
 
-func TestSafeIdCard(t *testing.T) {
-	assert.Equalf(t, "4602 **** **** 0027", SafeIdCard("460220202012210027"), "failed")
+func TestIdCard(t *testing.T) {
+	assert.Equalf(t, "4602 **** **** 0027", IdCard("460220202012210027"), "failed")
 }
 
-func TestSafeMobile(t *testing.T) {
-	assert.Equalf(t, "138****1234", SafeMobile("13800001234"), "failed")
+func TestIdCardStrict(t *testing.T) {
+	assert.Equalf(t, "4*** **** **** ***7", IdCardStrict("460220202012210027"), "failed")
 }
 
-func TestSafeChineseName(t *testing.T) {
-	assert.Equalf(t, "**", SafeChineseName("郑"), "failed")
-	assert.Equalf(t, "郑*", SafeChineseName("郑爽"), "failed")
-	assert.Equalf(t, "周*伦", SafeChineseName("周杰伦"), "failed")
-	assert.Equalf(t, "欧**娜", SafeChineseName("欧阳娜娜"), "failed")
-	assert.Equalf(t, "斯***乐", SafeChineseName("斯琴格日乐"), "failed")
-	assert.Equalf(t, "阿*********提", SafeChineseName("阿不来提 阿卜杜热西提"), "failed")
-	assert.Equalf(t, "C****多", SafeChineseName("C 罗纳尔多"), "failed")
+func TestMobile(t *testing.T) {
+	assert.Equalf(t, "138****1234", Mobile("13800001234"), "failed")
+}
+
+func TestLastFour(t *testing.T) {
+	assert.Equalf(t, "123", LastFour("123"), "failed")
+	assert.Equalf(t, "1234", LastFour("13800001234"), "failed")
+	assert.Equalf(t, "0027", LastFour("460220202012210027"), "failed")
+}
+
+func TestChineseName(t *testing.T) {
+	assert.Equalf(t, "**", ChineseName("郑"), "failed")
+	assert.Equalf(t, "*爽", ChineseName("郑爽"), "failed")
+	assert.Equalf(t, "*杰伦", ChineseName("周杰伦"), "failed")
+	assert.Equalf(t, "**娜娜", ChineseName("欧阳娜娜"), "failed")
+	assert.Equalf(t, "斯**日乐", ChineseName("斯琴格日乐"), "failed")
+	assert.Equalf(t, "阿********西提", ChineseName("阿不来提 阿卜杜热西提"), "failed")
+	assert.Equalf(t, "C***尔多", ChineseName("C 罗纳尔多"), "failed")
 
 }
 
 func TestSafeEmail(t *testing.T) {
-	assert.Equalf(t, "*@g.com", SafeEmail("1@g.com"), "failed")
-	assert.Equalf(t, "1*@tsinghua.org.cn", SafeEmail("10@tsinghua.org.cn"), "failed")
-	assert.Equalf(t, "1**@163.com", SafeEmail("100@163.com"), "failed")
-	assert.Equalf(t, "1***@foxmail.com", SafeEmail("1000@foxmail.com"), "failed")
-	assert.Equalf(t, "1****@sina.com.cn", SafeEmail("10000@sina.com.cn"), "failed")
-	assert.Equalf(t, "1**********@gmail.com", SafeEmail("13312345678@gmail.com"), "failed")
+	assert.Equalf(t, "*@g.com", Email("1@g.com"), "failed")
+	assert.Equalf(t, "1*@tsinghua.org.cn", Email("10@tsinghua.org.cn"), "failed")
+	assert.Equalf(t, "1**@163.com", Email("100@163.com"), "failed")
+	assert.Equalf(t, "1***@foxmail.com", Email("1000@foxmail.com"), "failed")
+	assert.Equalf(t, "1****@sina.com.cn", Email("10000@sina.com.cn"), "failed")
+	assert.Equalf(t, "1**********@gmail.com", Email("13312345678@gmail.com"), "failed")
 }
