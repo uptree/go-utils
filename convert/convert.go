98d28a908cc28504172c0c027f665e8ae24021b6
uptree <uptree@qq.com>
2022-08-22 11:47:25 +0800

    强制类型转换

---
+++ convert/convert.go
@@ -0,0 +1,126 @@
+package convert
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// AnyToString ...
+func AnyToString(i interface{}) string {
+	var s string
+	switch v := i.(type) {
+	case nil:
+		s = ""
+	case int:
+		s = strconv.Itoa(v)
+	case int8:
+		s = strconv.Itoa(int(v))
+	case int16:
+		s = strconv.Itoa(int(v))
+	case int32: // same as `rune`
+		s = strconv.Itoa(int(v))
+	case int64:
+		s = strconv.Itoa(int(v))
+	case uint:
+		s = strconv.FormatUint(uint64(v), 10)
+	case uint8:
+		s = strconv.FormatUint(uint64(v), 10)
+	case uint16:
+		s = strconv.FormatUint(uint64(v), 10)
+	case uint32:
+		s = strconv.FormatUint(uint64(v), 10)
+	case uint64:
+		s = strconv.FormatUint(v, 10)
+	case float32:
+		s = strconv.FormatFloat(float64(v), 'f', -1, 32)
+	case float64:
+		s = strconv.FormatFloat(v, 'f', -1, 64)
+	case bool:
+		s = strconv.FormatBool(v)
+	case string:
+		s = v
+	case []byte:
+		s = string(v)
+	case time.Duration:
+		s = v.String()
+	case json.Number:
+		s = v.String()
+	default:
+		s = fmt.Sprint(v)
+	}
+	return s
+}
+
+// IntToString int => string
+func IntToString(i int) string {
+	return strconv.Itoa(i)
+}
+
+// Uint64ToString uint64 => string
+func Uint64ToString(i uint64) string {
+	return strconv.FormatUint(i, 10)
+}
+
+// Float64ToString float64 => string
+func Float64ToString(f float64) string {
+	return strconv.FormatFloat(f, 'f', -1, 64)
+}
+
+// Float32ToString float32 => string
+func Float32ToString(f float32) string {
+	return strconv.FormatFloat(float64(f), 'f', -1, 32)
+}
+
+// StringToInt string => int
+func StringToInt(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+// StringToInt32 string => int32
+func StringToInt32(s string) int32 {
+	return int32(StringToInt64(s))
+}
+
+// StringToInt64 string => int64
+func StringToInt64(s string) int64 {
+	i, _ := strconv.ParseInt(s, 10, 0)
+	return i
+}
+
+// StringToUint64 string => uint64
+func StringToUint64(s string) uint64 {
+	i, _ := strconv.ParseUint(s, 10, 0)
+	return i
+}
+
+// IntToUint int => uint
+func IntToUint(i int) uint {
+	return uint(i)
+}
+
+// UintToInt uint => int
+func UintToInt(i uint) int {
+	return int(i)
+}
+
+// MapToJson map => json
+func MapToJson(m map[string]string) (string, error) {
+	b, e := json.Marshal(m)
+	if e != nil {
+		return "", e
+	}
+	return string(b), nil
+}
+
+// JsonToMap json => map
+func JsonToMap(s string) (map[string]string, error) {
+	m := make(map[string]string)
+	err := json.Unmarshal([]byte(s), &m)
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
---
+++ convert/convert_test.go
@@ -0,0 +1,67 @@
+package convert
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAnyToString(t *testing.T) {
+	assert.Equalf(t, "0", AnyToString(0), "failed")
+	assert.Equalf(t, "-123", AnyToString(-123), "failed")
+	assert.Equalf(t, "-123.123456", AnyToString(-123.123456), "failed")
+	assert.Equalf(t, "9223372036854775807", AnyToString(math.MaxInt64), "failed")
+}
+
+func TestIntToString(t *testing.T) {
+	assert.Equalf(t, "12345678", IntToString(12345678), "failed")
+	assert.Equalf(t, "-12345678", IntToString(-12345678), "failed")
+}
+
+func TestUint64ToString(t *testing.T) {
+	assert.Equalf(t, "12345678", Uint64ToString(uint64(12345678)), "failed")
+}
+
+func TestFloat64ToString(t *testing.T) {
+	assert.Equalf(t, "1.23456789", Float64ToString(1.23456789), "failed")
+}
+
+func TestFloat32ToString(t *testing.T) {
+	assert.Equalf(t, "1.2345679", Float32ToString(float32(1.23456789)), "failed")
+}
+
+func TestStringToInt(t *testing.T) {
+	assert.Equalf(t, 1234567, StringToInt("1234567"), "failed")
+}
+
+func TestStringToInt32(t *testing.T) {
+	assert.Equalf(t, int32(1234567), StringToInt32("1234567"), "failed")
+}
+
+func TestStringToInt64(t *testing.T) {
+	assert.Equalf(t, int64(1234567890123), StringToInt64("1234567890123"), "failed")
+}
+
+func TestStringToUint64(t *testing.T) {
+	assert.Equalf(t, uint64(1234567890123), StringToUint64("1234567890123"), "failed")
+}
+
+func TestIntToUint(t *testing.T) {
+	assert.Equalf(t, uint(1234567), IntToUint(1234567), "failed")
+}
+
+func TestUintToInt(t *testing.T) {
+	assert.Equalf(t, 1234567, UintToInt(1234567), "failed")
+}
+
+func TestMapToJson(t *testing.T) {
+	j, _ := MapToJson(map[string]string{"a": "b", "c": "d"})
+	assert.Equalf(t, "{\"a\":\"b\",\"c\":\"d\"}", j, "failed")
+}
+
+func TestJsonToMap(t *testing.T) {
+	m, _ := JsonToMap("{\"a\":\"b\",\"c\":\"d\"}")
+	assert.Equalf(t, "b", m["a"], "failed")
+	assert.Equalf(t, "d", m["c"], "failed")
+}
